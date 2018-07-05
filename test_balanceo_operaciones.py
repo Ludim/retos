44b6912b32b55919662321905e566f2303cd76e9
Ludim <ludim.anel@gmail.com>
2018-07-05 00:42:01 -0500

    Style[test_balanceo_operaciones.py]: Add print input, output {expected,function{

--- test_balanceo_operaciones.py
+++ test_balanceo_operaciones.py
@@ -8,8 +8,10 @@ class BalanceoOperacionesTest(unittest.TestCase):
                         ("{({))})]", False), ("[[()", False))
 
     def test_entrada_balanceada(self):
+        print()
         for input_, output_ in self.input_output:
             res = es_entrada_balanceada(input_)
+            print("Input={} | Expected_output={} | Function_output={}".format(input_,output_,res))
             self.assertEqual(output_, res)
 
 if __name__ == '__main__':
